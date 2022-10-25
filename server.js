70373a2e8051b5bf221281b73ed158af527b4d3f
Timothy Mei <timothymei3270@gmail.com>
2022-10-25 15:08:13 -0400

    untracked node modules, preparing for deployment

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/api', routes)
 
 app.get('/', (req, res) => {
   res.send('This is root!')
+})
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
 app.listen(PORT, () => {
