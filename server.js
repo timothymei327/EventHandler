d1e93431c95eeb862399e40e6ea5f910987efce7
Timothy Mei <timothymei3270@gmail.com>
2022-12-02 17:10:13 -0500

    wired up code for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
 const routes = require('./routes')
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 const db = require('./db')
 
 const app = express()
@@ -10,7 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
-// app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/api', routes)
 
@@ -18,9 +18,9 @@ app.get('/', (req, res) => {
   res.send('This is root!')
 })
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(`${__dirname}/client/build/index.html`)
-// })
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
